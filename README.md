3893290ded1f4cd7bd09f48d1cda300a070b5b5d
Djeison Selzlein <djeisonaguir@gmail.com>
2017-10-30 15:51:07 -0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-websockets-rabbitmq-demo
---
+++ src/main/java/com/selzlein/djeison/springwebsocketsrabbitmqdemo/SpringWebsocketsRabbitmqDemoApplication.java
@@ -0,0 +1,12 @@
+package com.selzlein.djeison.springwebsocketsrabbitmqdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsocketsRabbitmqDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebsocketsRabbitmqDemoApplication.class, args);
+	}
+}
